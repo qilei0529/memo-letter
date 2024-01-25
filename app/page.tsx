c4e199c67f2dc0431e22ba2afeefd8b1ea857505
QL wei <qilei0529@gmail.com>
2024-01-25 10:51:57 +0800

    feat: update code

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-
 import Image from "next/image";
-
-
+import { CardView } from "@/components/card";
 
 export default function Home(props: any) {
 
-  return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="relative flex flex-row">
-        <div className="w-8 h-8 flex items-center justify-center cursor-pointer bg-gray-100 hover:bg-gray-500">12</div>
-        <div className="ml-2 w-8 h-8 flex items-center justify-center cursor-pointer bg-gray-100 hover:bg-gray-500">10</div>
-      </div>
-      <textarea className="editor the_font_10 w-[600px] h-[600px]" placeholder="写点什么吧">
-      </textarea>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center pt-24 sm:p-24">
+            <span className="the_font_10">A tool for writing letter in pixel.</span>
+            <CardView />
+        </main>
+    );
 }
